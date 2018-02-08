f70d5985939d9ef732b1b510a23091b40963563a
Dariaaa <darikarpenko@yandex.ru>
2018-02-08 22:01:26 +0300

    addded creating constraint and index in database

--- src/db/postgres_db_init.py
+++ src/db/postgres_db_init.py
@@ -27,7 +27,6 @@ class DBInitialisator:
     def create_schema_ddl(self,schema: Schema):
         return "CREATE SCHEMA {};".format(schema.name)
 
-
     def create_domain_ddl(self, domain:Domain, schema:Schema):
         ddl =  """CREATE DOMAIN {}."{}" AS {}; """\
             .format(schema.name, domain.name, get_type_in_postgres(domain))
@@ -50,25 +49,76 @@ class DBInitialisator:
         return ",".join([self.create_field_ddl(field,schema)
                            for field in table.fields])
 
+    def create_constraint_ddl(self,constraint:Constraint,table:Table,schema:Schema):
+        details = []
+        for det in constraint.details:
+            detail = r'"' + det.value + r'"'
+            details.append(detail)
+
+        if constraint.kind.lower() == 'primary':
+            str = """PRIMARY KEY ({})"""\
+                .format(', '.join(details))
+        elif constraint.kind.lower() == 'foreign':
+            str = """FOREIGN KEY ({}) REFERENCES {}."{}" DEFERRABLE"""\
+                .format(', '.join(details),schema.name,
+                    constraint.reference,constraint.name)
+        else: return ''
+
+        return """ALTER TABLE {}."{}" ADD {};"""\
+            .format(schema.name,table.name,str)
+
+    
+    def create_index_ddl(self,index: Index, table: Table, schema: Schema):
+        details = []
+        for det in index.details:
+            detail = '\"' + det.value + '\"'
+            if det.expression:
+                detail += ' (' + det.expression + ')'
+            if not det.descend:
+                detail += ' ASC'
+            else:
+                detail += det.descend.upper()
+            details.append(detail)
+
+        if len(details) == 0:
+            return ''
+
+        return """CREATE INDEX {} ON {}."{}"({});"""\
+            .format('"' + index.name + table.name + '"' if index.name else '',
+                    schema.name,table.name,', '.join(details))
+
+
     def start(self, database_name:str, schema:Schema):
         self.create_database(database_name)
+
         scripts = []
+
         scripts.append('BEGIN TRANSACTION;')
         scripts.append(self.create_schema_ddl(schema))
         scripts.append('\n'.join([
             self.create_domain_ddl(d,schema)
             for d in schema.domains
         ]))
-        scripts.append('\n'.join([
-            self.create_table_ddl(t, schema)
-            for t in schema.tables
-        ]))
 
+        foreign = []
+
+        for table in schema.tables:
+            scripts.append(self.create_table_ddl(table, schema))
+
+            for index in table.indexes:
+                scripts.append(self.create_index_ddl(index,table,schema))
+
+            for constraint in table.constraints:
+                if constraint.kind.lower() == 'foreign':
+                    foreign.append(self.create_constraint_ddl(constraint,table,schema))
+                else:
+                    scripts.append(self.create_constraint_ddl(constraint,table,schema))
+
+        scripts.append('\n'.join(foreign))
         scripts.append('COMMIT;')
 
-
         queries = '\n'.join(scripts)
-        print(queries)
+
         self.conn.execute(queries)
 
 
